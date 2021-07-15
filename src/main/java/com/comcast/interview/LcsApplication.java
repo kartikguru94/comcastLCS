2a1f0276747b69b4469500485edade4a62c8170c
aguru@dminc.com <aguru@dminc.com>
2021-07-15 10:28:03 +0530

    Build UI added

--- src/main/java/com/comcast/interview/LcsApplication.java
+++ src/main/java/com/comcast/interview/LcsApplication.java
@@ -2,12 +2,24 @@ package com.comcast.interview;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class LcsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LcsApplication.class, args);
+	}
+	
+	@Configuration
+	public class WebConfiguration implements WebMvcConfigurer {
+
+	    @Override
+	    public void addCorsMappings(CorsRegistry registry) {
+	        registry.addMapping("/**").allowedMethods("*");
+	    }
 	}
 	
 }
